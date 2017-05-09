6952db47b7bcee96e9b52bf6b77fac4658984e30
FourmiZ <271486988@qq.com>
2017-05-09 22:33:30 +0800

    初始化项目

---
+++ WeChat_UI_Demo/WeChat_UI_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat_UI_Demo
+//
+//  Created by ZeZhong on 2017/5/9.
+//  Copyright © 2017年 FourmiZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat_UI_Demo/WeChat_UI_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeChat_UI_Demo
+//
+//  Created by ZeZhong on 2017/5/9.
+//  Copyright © 2017年 FourmiZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
